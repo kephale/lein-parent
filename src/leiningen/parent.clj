a0b67deeb98c5cc09f2d02f55a9c9a21a384c635
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-09-24 11:43:52 -0400

    Debug info when resolving coords.

--- src/leiningen/parent.clj
+++ src/leiningen/parent.clj
@@ -53,6 +53,7 @@
 
 (defn resolve-project-from-coords
   [coords {:keys [repositories offline? update checksum]}]
+  (println :resolve-project-from-coords coords repositories)
   (let [resolved-parent-artifact (first (aether/resolve-artifacts
                                           :coordinates [coords]
                                           :repositories (map (comp (partial update-policies update checksum) classpath/add-repo-auth)
